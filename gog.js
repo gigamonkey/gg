a20ab8806a0da0bd7cf0fa23db62f76141b62ebb
Peter Seibel <peter@etsy.com>
2012-07-23 13:37:57 -0700

    Kludgy introduction of some scaling.

--- gog.js
+++ gog.js
@@ -13,11 +13,11 @@
 
     Graph.prototype.render = function (id, data) {
         // Render the graph using the given data into the div with the given id.
-        var svg = d3.select(id).append('svg')
+        this.svg = d3.select(id).append('svg')
             .attr('width', this.width)
             .attr('height', this.height);
 
-        svg.append('rect')
+        this.svg.append('rect')
             .attr('x', 0)
             .attr('y', 0)
             .attr('width', this.width)
@@ -25,7 +25,16 @@
             .attr('fill', '#aaa')
             .attr('fill-opacity', 1);
 
-        _.each(this.elements, function (e) { e.render(svg, data); })
+        var xMin = _.min(data, function (d) { return d.d; }).d;
+        var xMax = _.max(data, function (d) { return d.d; }).d;
+
+        var yMin = _.min(data, function (d) { return d.r; }).r;
+        var yMax = _.max(data, function (d) { return d.r; }).r;
+
+        this.xscale = d3.scale.linear().domain([xMin, xMax]).range([0, this.width]);
+        this.yscale = d3.scale.linear().domain([yMin, yMax]).range([this.height, 0]);
+
+        _.each(this.elements, function (e) { e.render(this, data); }, this)
     }
 
     Graph.prototype.element = function (e) {
@@ -38,13 +47,14 @@
         return this;
     }
 
-    PointElement.prototype.render = function (svg, data) {
-        var circle = svg.selectAll('circle')
+    PointElement.prototype.render = function (graph, data) {
+        var that = this;
+        var circle = graph.svg.selectAll('circle')
             .data(data)
             .enter()
             .append('circle')
-            .attr('cx', this.xFn)
-            .attr('cy', this.yFn)
+            .attr('cx', function (d) { return graph.xscale(that.xFn(d)) })
+            .attr('cy', function (d) { return graph.yscale(that.yFn(d)) })
             .attr('r', this.rFn);
     }
 
@@ -52,9 +62,13 @@
         return this;
     }
 
-    LineElement.prototype.render = function (svg, data) {
-        var polyline = svg.append('polyline')
-            .attr('points', _.map(data, function (d) { return this.xFn(d) + ',' + this.yFn(d); }, this).join(' '))
+    LineElement.prototype.render = function (graph, data) {
+        var e = this;
+        function x (d) { return graph.xscale(e.xFn(d)); }
+        function y (d) { return graph.yscale(e.yFn(d)); }
+
+        var polyline = graph.svg.append('polyline')
+            .attr('points', _.map(data, function (d) { return x(d) + ',' + y(d); }, this).join(' '))
             .attr('fill', 'none')
             .attr('stroke', 'black')
             .attr('stroke-width', 2);
@@ -112,7 +126,7 @@
         var y = 0;
         _.times(20, function () {
             x += Math.random() * 30;
-            y += Math.random() * 30;
+            y += 20  - Math.random() * 30;
             data.push({
                 d: x,
                 r: y,
@@ -122,8 +136,8 @@
     }());
 
     $(document).ready(function() {
-        graph(size('500px', '300px')).element(point(position('d*r'))).render('#example1', data);
-        graph(size('500px', '300px')).element(line(position('d*r'))).render('#example2', data);
+        graph(size('500', '300')).element(point(position('d*r'))).render('#example1', data);
+        graph(size('500', '300')).element(line(position('d*r'))).render('#example2', data);
     });
 
 })();
