df4e377196d9c6f532765ffb2ea3e0a4fa951ae6
Peter Seibel <peter@etsy.com>
2012-07-23 11:50:38 -0700

    Draw a few random circles.

--- gog.js
+++ gog.js
@@ -2,6 +2,7 @@
 
     function Graph () {
         // Make a graph object that knows how to render itself using d3.
+        this.elements = [];
         return this;
     }
 
@@ -23,15 +24,32 @@
             .attr('height', this.height)
             .attr('fill', '#aaa')
             .attr('fill-opacity', 1);
+
+        _.each(this.elements, function (e) { e.render(svg, data); })
     }
 
+    Graph.prototype.element = function (e) {
+        this.elements.push(e);
+    }
+
+    function PointElement () {
+        this.rFn = function (d) { return 10; };
+        return this;
+    }
+
+    PointElement.prototype.render = function (svg, data) {
+        var circle = svg.selectAll('circle')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('cx', this.xFn)
+            .attr('cy', this.yFn)
+            .attr('r', this.rFn);
+    }
+
+    // Make a graph
     function graph() {
-        // Make a graph
-        var g = new Graph();
-        for (var i = 0; i < arguments.length; i++) {
-            arguments[i](g);
-        }
-        return g;
+        return applyArguments(arguments, new Graph());
     }
 
     // Set the size of a graph.
@@ -39,9 +57,33 @@
         return function (g) { g.setSize(w, h); }
     }
 
+    function position(expr) {
+        return function (e) {
+            e.xFn = function (d) { return Math.random() * 200; }
+            e.yFn = function (d) { return Math.random() * 300; }
+        }
+    }
+
+    function point () {
+        return applyArguments(arguments, new PointElement());
+    }
+
+    function applyArguments (args, obj) {
+        _.each(args, function (fn) { fn(obj); });
+        return obj;
+    }
+
+    var data = [
+        { d: 10, r: 20 },
+        { d: 12, r: 30 },
+        { d: 20, r: 6 },
+    ];
+
+
     $(document).ready(function() {
         var g = graph(size('500px', '300px'));
-        g.render('#g1', null);
+        g.element(point(position('d*r')));
+        g.render('#g1', data);
     });
 
 })();
