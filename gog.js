f0f476bb9edbfb768abd46858cbdb406b9f5e550
Peter Seibel <peter@etsy.com>
2012-07-24 09:57:33 -0700

    Finished removing old unexportable API.

--- gog.js
+++ gog.js
@@ -61,20 +61,20 @@
         // Default to linear scales if not supplied.
         for (var i = 1; i < 3; i++) {
             if (typeof this.scales[i] === 'undefined') {
-                this.scale(linear(dim(i)))
+                this.scale(new LinearScale().dim(i));
             }
         }
 
         // Default the scale's domains if they are not supplied.
         _.each(this.scales, function (s, dim) {
             if (! s.domainSet) {
-                if (typeof s.min === 'undefined') {
-                    s.min = (dim == 1) ? this.xMin(data) : this.yMin(data);
+                if (typeof s._min === 'undefined') {
+                    s._min = (dim == 1) ? this.xMin(data) : this.yMin(data);
                 }
-                if (typeof s.max === 'undefined') {
-                    s.max = (dim == 1) ? this.xMax(data) : this.yMax(data);
+                if (typeof s._max === 'undefined') {
+                    s._max = (dim == 1) ? this.xMax(data) : this.yMax(data);
                 }
-                this.d3Scales[dim] = s.d3Scale.domain([s.min, s.max]).range(this.rangeForDim(dim));
+                this.d3Scales[dim] = s.d3Scale.domain([s._min, s._max]).range(this.rangeForDim(dim));
             } else {
                 this.d3Scales[dim] = s.d3Scale.rangeBands(this.rangeForDim(dim), .5);
             }
@@ -89,114 +89,13 @@
     };
 
     Graph.prototype.scale = function (s) {
-        this.scales[s.dim] = s;
+        this.scales[s._dim] = s;
         return this;
     };
 
-    function PointElement () {
-        this.rFn = function (d) { return 5; };
-        return this;
-    }
+    function Element () { return this; }
 
-    PointElement.prototype.render = function (graph, data) {
-        var that = this;
-        var circle = graph.svg.append('g').selectAll('circle')
-            .data(data)
-            .enter()
-            .append('circle')
-            .attr('cx', function (d) { return graph.d3Scales[1](that.xFn(d)) })
-            .attr('cy', function (d) { return graph.d3Scales[2](that.yFn(d)) })
-            .attr('r', this.rFn);
-    };
-
-    function LineElement () { return this; }
-
-    LineElement.prototype.render = function (graph, data) {
-        var e = this;
-        function x (d) { return graph.d3Scales[1](e.xFn(d)); }
-        function y (d) { return graph.d3Scales[2](e.yFn(d)); }
-
-        var polyline = graph.svg.append('polyline')
-            .attr('points', _.map(data, function (d) { return x(d) + ',' + y(d); }, this).join(' '))
-            .attr('fill', 'none')
-            .attr('stroke', 'black')
-            .attr('stroke-width', 2);
-    }
-
-    function IntervalElement () { return this; }
-
-    IntervalElement.prototype.render = function (graph, data) {
-        var that = this;
-        var rect = graph.svg.append('g').selectAll('rect')
-            .data(data)
-            .enter()
-            .append('rect')
-            .attr('x', function (d) { return graph.d3Scales[1](that.xFn(d)) - 2.5; })
-            .attr('width', 5)
-            .attr('y', function (d) { return graph.d3Scales[2](that.yFn(d)); })
-            .attr('height', function (d) { return graph.d3Scales[2](graph.scales[2].min) - graph.d3Scales[2](that.yFn(d)); });
-    };
-
-    // Scales
-
-    function LinearScale () {
-        this.d3Scale = d3.scale.linear();
-        return this;
-    }
-
-    function LogScale () {
-        this.d3Scale = d3.scale.log();
-        return this;
-    }
-
-    function CategoricalScale () {
-        this.d3Scale = d3.scale.ordinal();
-        return this;
-    }
-
-    ////////////////////////////////////////////////////////////////////////
-    /// API
-
-    // Elements
-
-    function point () { return build(PointElement, arguments); }
-
-    function line () { return build(LineElement, arguments); }
-
-    function interval () { return build(IntervalElement, arguments); }
-
-    // Scales
-
-    function linear () { return build(LinearScale, arguments); }
-
-    function log () { return build(LogScale, arguments); }
-
-    function cat () { return build(CategoricalScale, arguments); }
-
-    // Set the dimension of a scale.
-    function dim (v) {
-        return function (scale) { scale.dim = v; }
-    }
-
-    function values () {
-        var d = arguments;
-        return function (scale) {
-            scale.domainSet = true;
-            scale.d3Scale.domain(d);
-        }
-    }
-
-    // Set the minimum value of a scale.
-    function min (v) {
-        return function (scale) { scale.min = v; }
-    }
-
-    // Set the maximum value of a scale.
-    function max (v) {
-        return function (scale) { scale.max = v; }
-    }
-
-    function position(expr) {
+    Element.prototype.position = function (expr) {
         var parse = expr.split(/([*\/+])/);
         var operands = [];
         var operators = [];
@@ -212,22 +111,110 @@
             throw "/ and + not implemented.";
         }
 
-        return function (e) {
-            e.xFn = function (d) { return d[operands[0]]; }
-            e.yFn = function (d) { return d[operands[1]]; }
-        }
+        this.xFn = function (d) { return d[operands[0]]; }
+        this.yFn = function (d) { return d[operands[1]]; }
+        return this;
+    }
+
+
+    function PointElement () {
+        this.rFn = function (d) { return 5; };
+        return this;
+    }
+
+    PointElement.prototype = new Element();
+
+    PointElement.prototype.render = function (graph, data) {
+        var that = this;
+        var circle = graph.svg.append('g').selectAll('circle')
+            .data(data)
+            .enter()
+            .append('circle')
+            .attr('cx', function (d) { return graph.d3Scales[1](that.xFn(d)) })
+            .attr('cy', function (d) { return graph.d3Scales[2](that.yFn(d)) })
+            .attr('r', this.rFn);
+    };
+
+    function LineElement () { return this; }
+
+    LineElement.prototype = new Element();
+
+    LineElement.prototype.render = function (graph, data) {
+        var e = this;
+        function x (d) { return graph.d3Scales[1](e.xFn(d)); }
+        function y (d) { return graph.d3Scales[2](e.yFn(d)); }
+
+        var polyline = graph.svg.append('polyline')
+            .attr('points', _.map(data, function (d) { return x(d) + ',' + y(d); }, this).join(' '))
+            .attr('fill', 'none')
+            .attr('stroke', 'black')
+            .attr('stroke-width', 2);
+    }
+
+    function IntervalElement () { return this; }
+
+    IntervalElement.prototype = new Element();
+
+    IntervalElement.prototype.render = function (graph, data) {
+        var that = this;
+        var rect = graph.svg.append('g').selectAll('rect')
+            .data(data)
+            .enter()
+            .append('rect')
+            .attr('x', function (d) { return graph.d3Scales[1](that.xFn(d)) - 2.5; })
+            .attr('width', 5)
+            .attr('y', function (d) { return graph.d3Scales[2](that.yFn(d)); })
+            .attr('height', function (d) { return graph.d3Scales[2](graph.scales[2]._min) - graph.d3Scales[2](that.yFn(d)); });
+    };
+
+    // Scales
+
+    function Scale () { return this; }
+
+    Scale.prototype.dim = function (d) {
+        this._dim = d;
+        return this;
+    }
+
+    Scale.prototype.min = function (m) {
+        this._min = m;
+        return this;
+    }
+
+    Scale.prototype.max = function (m) {
+        this._max = m;
+        return this;
+    }
+
+    function LinearScale () {
+        this.d3Scale = d3.scale.linear();
+        return this;
+    }
+
+    LinearScale.prototype = new Scale();
+
+    function LogScale () {
+        this.d3Scale = d3.scale.log();
+        return this;
+    }
+
+    LogScale.prototype = new Scale();
+
+    function CategoricalScale () {
+        this.d3Scale = d3.scale.ordinal();
+        return this;
+    }
+
+    CategoricalScale.prototype = new Scale();
+
+    CategoricalScale.prototype.values = function () {
+        this.domainSet = true;
+        this.d3Scale.domain(arguments);
+        return this;
     }
 
     ////////////////////////////////////////////////////////////////////////
-    /// Internals
-
-    // This is a kind of goofy way to do things but it enables us to
-    // follow the Graphics Production Language pretty closely.
-    function build (constructor, args) {
-        var obj = new constructor();
-        _.each(args, function (fn) { fn(obj); });
-        return obj;
-    }
+    /// Examples
 
     // Generate some random data.
     var data = (function () {
@@ -278,38 +265,39 @@
         var w = 250;
         var h = 150;
 
-        var g = new Graph().size(250, 150);
-        var p = new PointElement();
-        position('d*r')(p);
-        g.element(p).render(ex(), data);
-
         // scatterplot
-        new Graph().size(w, h).element(point(position('d*r'))).render(ex(), data);
+        new Graph().size(w, h)
+            .element(new PointElement().position('d*r'))
+            .render(ex(), data);
 
         // line chart
-        new Graph().size(w, h).element(line(position('d*r'))).render(ex(), data);
+        new Graph().size(w, h)
+            .element(new LineElement().position('d*r'))
+            .render(ex(), data);
 
         // bar chart
-        new Graph().size(w, h).element(interval(position('d*r'))).render(ex(), data);
+        new Graph().size(w, h)
+            .element(new IntervalElement().position('d*r'))
+            .render(ex(), data);
 
         // histogram
         new Graph().size(w, h)
-            .element(interval(position('category*count')))
-            .scale(cat(dim(1), values('foo', 'bar', 'baz', 'quux')))
-            .scale(linear(dim(2), min(0)))
+            .element(new IntervalElement().position('category*count'))
+            .scale(new CategoricalScale().dim(1).values('foo', 'bar', 'baz', 'quux'))
+            .scale(new LinearScale().dim(2).min(0))
             .render(ex(), categoricalData);
 
         // combined points and line
         new Graph().size(w, h)
-            .element(point(position('d*r')))
-            .element(line(position('d*r')))
+            .element(new PointElement().position('d*r'))
+            .element(new LineElement().position('d*r'))
             .render(ex(), data);
 
         // semi-log scale
         new Graph().size(w, h)
-            .element(point(position('d*r')))
-            .element(line(position('d*r')))
-            .scale(log(dim(2)))
+            .element(new PointElement().position('d*r'))
+            .element(new LineElement().position('d*r'))
+            .scale(new LogScale().dim(2))
             .render(ex(), semiLogData);
 
     });
