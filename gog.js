ff82728ae626b992b059d5c4f3117101446b358a
Peter Seibel <peter@etsy.com>
2012-07-24 17:13:12 -0700

    Breaking out examples.js

--- gog.js
+++ gog.js
@@ -1,4 +1,4 @@
-;(function () {
+;(function (exports) {
 
     var _undefined;
 
@@ -140,8 +140,8 @@
 
     ////////////////////////////////////////////////////////////////////////
     // Geometry objects are the ones that actually draw stuff onto the
-    // Graphic. They only care about scaled values.
-
+    // Graphic. They only care about scaled values which they can get
+    // from their layer.
 
     function Geometry () { return this; }
 
@@ -314,116 +314,7 @@
         return g;
     }
 
-    window.gg = gg;
-
-    ////////////////////////////////////////////////////////////////////////
-    /// Examples
-
-    // Generate some random data.
-    var data = (function () {
-        var data = [];
-        var x = 0;
-        var y = 0;
-        _.times(20, function () {
-            x += Math.random() * 30;
-            y += 20 - Math.random() * 30;
-            data.push({
-                d: x,
-                r: y,
-            });
-        });
-        return data;
-    }());
-
-    // Generate some random data for plotting semi-log.
-    var semiLogData = (function () {
-        var data = [];
-        var x = 0;
-        var y = 1;
-        _.times(20, function () {
-            x += Math.random() * 30;
-            y *= Math.random() * 5;
-            data.push({
-                d: x,
-                r: y,
-            });
-        });
-        return data;
-    }());
+    exports.gg = gg;
 
 
-    // Some categorical data
-    var categoricalData = [
-        { category: 'foo', count: 100 },
-        { category: 'bar', count: 59 },
-        { category: 'baz', count: 212 },
-        { category: 'quux', count: 76 }
-    ];
-
-
-    $(document).ready(function() {
-
-        function ex () { return d3.select('#examples').append('span'); }
-
-        var w = 250;
-        var h = 150;
-
-        // Define graphics ...
-        var scatterplot = gg({
-            width: w,
-            height: h,
-            layers: [{ geometry: 'point', mapping: { x: 'd', y: 'r' } }]
-        });
-
-        var linechart = gg({
-            width: w,
-            height: h,
-            layers: [{ geometry: 'line', mapping: { x: 'd', y: 'r' } }]
-        });
-
-        var barchart = gg({
-            width: w,
-            height: h,
-            layers: [{ geometry: 'interval', mapping: { x: 'd', y: 'r' } }]
-        });
-
-        var histogram = gg({
-            width: w,
-            height: h,
-            layers: [{ geometry: 'interval', mapping: { x: 'category', y: 'count' } }],
-            scales: [
-                { type: 'categorical', aesthetic: 'x', values: ['foo', 'bar', 'baz', 'quux'] },
-                { type: 'linear', aesthetic: 'y', min: 0 }
-            ]
-        });
-
-        var combined_points_and_line = gg({
-            width: w,
-            height: h,
-            layers: [
-                { geometry: 'point', mapping: { x: 'd', y: 'r' } },
-                { geometry: 'line', mapping: { x: 'd', y: 'r' } },
-            ],
-        });
-
-        var semi_log_scale = gg({
-            width: w,
-            height: h,
-            layers: [
-                { geometry: 'point', mapping: { x: 'd', y: 'r' } },
-                { geometry: 'line', mapping: { x: 'd', y: 'r' } },
-            ],
-            scales: [ { type: 'log', aesthetic: 'y' } ]
-        });
-
-        // ... and render 'em
-        scatterplot.render(ex(), data);
-        linechart.render(ex(), data);
-        barchart.render(ex(), data);
-        histogram.render(ex(), categoricalData);
-        combined_points_and_line.render(ex(), data);
-        semi_log_scale.render(ex(), semiLogData);
-
-    });
-
-})();
+})(window);
