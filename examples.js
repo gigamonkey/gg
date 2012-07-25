72e2b44a208e86d8f33e885a549cab930b6733f3
Peter Seibel <peter@etsy.com>
2012-07-25 11:06:09 -0700

    Use d3.random.normal for random numbers.

--- examples.js
+++ examples.js
@@ -41,32 +41,10 @@
         { category: 'quux', count: 76 }
     ];
 
-    // From http://www.protonfish.com/jslib/boxmuller.shtml
-    function rnd_bmt() {
-        var x = 0, y = 0, rds, c;
-
-        // Get two random numbers from -1 to 1.
-        // If the radius is zero or greater than 1, throw them out and pick two new ones
-        // Rejection sampling throws away about 20% of the pairs.
-        do {
-            x = Math.random()*2-1;
-            y = Math.random()*2-1;
-            rds = x*x + y*y;
-        }
-        while (rds == 0 || rds > 1)
-
-        // This magic is the Box-Muller Transform
-        c = Math.sqrt(-2*Math.log(rds)/rds);
-
-        // It always creates a pair of numbers. I'll return them in an
-        // array. This function is quite efficient so don't be afraid
-        // to throw one away if you don't need both.
-        return [x*c, y*c];
-    }
-
-    function gaussian (mean, stddev) {
-        return mean + rnd_bmt()[0] * stddev;
-    }
+    // Some random variables.
+    var randomHeight   = d3.random.normal(66, 18);
+    var randomWeight   = d3.random.normal(200, 50);
+    var standardNormal = d3.random.normal();
 
     // Some data to be plotted with a histograpm
     var heightWeight = (function () {
@@ -74,14 +52,14 @@
         _.times(20000, function () {
             data.push({
                 // These should really be correlated.
-                height: gaussian(66, 18), // in inches
-                weight: gaussian(200, 50), // in lbs
+                height: randomHeight(), // in inches
+                weight: randomWeight(), // in lbs
             });
         });
         return data;
     }());
 
-    var normalData = _.map(_.range(20000), function (i) { return { v: gaussian(0, 1) }; });
+    var normalData = _.map(_.range(20000), function (i) { return { v: standardNormal() }; });
 
     $(document).ready(function() {
 
