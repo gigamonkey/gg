b4ca8f8f36dfbc5de8a0a028ac37fb361a1fd26b
Peter Seibel <peter@etsy.com>
2012-07-28 14:47:24 -0700

    Get some stats about the code for graphing.

---
+++ codestats.pl
@@ -0,0 +1,37 @@
+#!/usr/bin/env perl
+
+use warnings;
+use strict;
+
+my %excludes = (
+    'd3.v2.min.js'        => 1,
+    'jquery-1.7.2.min.js' => 1,
+    'json2.js'            => 1,
+    'underscore-min.js'   => 1,
+    );
+
+system("git co master 2> /dev/null") == 0 or die "checkout failed: $?";
+
+open(IN, "git log --pretty='format:%H %at %s' |") or die $!;
+my @loglines = <IN>;
+close IN;
+
+foreach (@loglines) {
+    chomp;
+    my ($sha, $utc, $subject) = split ' ', $_, 3;
+
+    system("git co $sha 2> /dev/null") == 0 or die "checkout failed: $?";
+
+    opendir(DIR, ".") or die $!;
+    my @files = grep { /.js$/ and not $excludes{$_} } readdir(DIR);
+    closedir DIR;
+
+    my $lines = @files ? `cat @files | wc -l` : '0';
+    $lines =~ s/\s*(\S+)\s*/$1/;
+
+    print "/* $subject files: @files */\n";
+    print "{ commit: '$sha', utc: $utc, lines: $lines }\n";
+    system("git co master 2> /dev/null") == 0 or die "checkout failed: $?";
+}
+
+__END__
